20736fc01126686aa04ff7225803e09b580d00a5
Cicin2837512 <CicinGithub@skiff.com>
2023-12-17 16:55:11 +0100

    Removed folders not supposed to be in wofi folder

--- wofi/qtile/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-picom -b
-nitrogen --restore &
-nm-applet &
-/usr/libexec/polkit-kde-authentication-agent 1 &
-xrandr --output HDMI-0 --right-of DVI-D-0
-xrandr --output HDMI-0 --primary
